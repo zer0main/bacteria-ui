c32cf9668a6c4498d46843fa14681829c0b83374
Pavel Dolgov <pdolgov99@gmail.com>
2016-03-12 17:24:05 +0300

    Add some GUI constants

---
+++ src/gui/constants.hpp
@@ -0,0 +1,21 @@
+/*
+ * bacteria-ui, GUI for cellular automaton
+ * Copyright (C) 2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef CONSTANTS_HPP_
+#define CONSTANTS_HPP_
+
+namespace Gui {
+
+/** The proportion of a team color's width in a cell */
+const double COLOR_WIDTH_PROPORTION = 0.333;
+
+/** The proportion of a team color's height in a cell */
+const double COLOR_HEIGHT_PROPORTION = 0.125;
+
+}
+
+#endif
