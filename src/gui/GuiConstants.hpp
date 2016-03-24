5145c72ff8e565998bfc7906672677f786de3c8d
Pavel Dolgov <pdolgov99@gmail.com>
2016-03-24 18:49:48 +0300

    Add GuiConstants.hpp
    INDENT, MAX_BACTERIA, MAX_TEAMS

---
+++ src/gui/GuiConstants.hpp
@@ -0,0 +1,21 @@
+/*
+ * bacteria-ui, GUI for cellular automaton
+ * Copyright (C) 2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+
+#ifndef GUI_CONSTANTS_HPP
+#define GUI_CONSTANTS_HPP
+
+/** Indent between minimum and default width/height */
+const int INDENT = 15;
+
+/** Maximum initial number of bacteria per team */
+const int MAX_BACTERIA = 1000;
+
+/** Maximum number of teams */
+const int MAX_TEAMS = 100;
+
+#endif
